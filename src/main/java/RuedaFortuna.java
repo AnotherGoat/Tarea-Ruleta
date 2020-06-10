d1ffb9138dfa05a6e1dd26498954c052688e7cd4
VictorMardones <v,mardones04@ufromail.cl>
2020-06-10 16:50:52 -0400

    Se completaron los métodos de la tarea

--- src/main/java/RuedaFortuna.java
+++ src/main/java/RuedaFortuna.java
@@ -1,16 +1,63 @@
+import java.util.Random;
+
 public class RuedaFortuna {
 
+    /**
+     * Casillas de la ruleta
+     */
     private int[] casillas;
 
+    /**
+     * Inializa cada casilla de la ruleta con un valor al azar
+     * @param numeroCasillas Número de casillas que tendrá la ruleta
+     */
     public void inicializarRueda(int numeroCasillas){
         if(numeroCasillas<4 || numeroCasillas>100){
             System.out.println("Error: número de casillas no válido");
         }
+
+        else{
+            casillas = new int[numeroCasillas];
+            System.out.println("La ruleta tiene los siguientes números: ");
+
+            // Llena las casillas con números al azar y muestra los números
+            for(int i=0; i<numeroCasillas; i++){
+                casillas[i] = randomInt();
+                System.out.print(casillas[i] + " ");
+            }
+
+            System.out.println("");
+        }
     }
 
-    public int lanzarRueda(){
+    /**
+     * Retorna un int al azar
+     * @return Número al azar
+     */
+    public int randomInt(){
+        Random num = new Random();
+        return num.nextInt();
+    }
 
-        return 0;
+    /**
+     * Método para lanzar la rueda y obtener el puntaje de una casilla
+     * @return Puntaje obtenido
+     */
+    public int lanzarRueda(){
+        int puntajeObtenido = casillas[randomEntre(0 , casillas.length-1)];
+        System.out.println("Ha obtenido "+puntajeObtenido+" puntos");
+        return puntajeObtenido;
+    }
+
+    /**
+     * Retorna un número al azar entre el rango ingresado
+     * @param min Valor mínimo del número al azar
+     * @param max Valor máximo del número al azar
+     * @return Número al azar
+     */
+    public int randomEntre(int min, int max){
+        Random num = new Random();
+        return min + num.nextInt(max-min);
     }
 
 }

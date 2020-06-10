0a1676af8536ff4e52ce7421b3a59e3d9c70bfd7
VictorMardones <v,mardones04@ufromail.cl>
2020-06-10 16:33:52 -0400

    Estructura básica

--- src/main/java/RuedaFortuna.java
+++ src/main/java/RuedaFortuna.java
@@ -1,5 +1,16 @@
 public class RuedaFortuna {
 
-    
+    private int[] casillas;
+
+    public void inicializarRueda(int numeroCasillas){
+        if(numeroCasillas<4 || numeroCasillas>100){
+            System.out.println("Error: número de casillas no válido");
+        }
+    }
+
+    public int lanzarRueda(){
+
+        return 0;
+    }
 
 }
